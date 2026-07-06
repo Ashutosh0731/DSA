673d1ec98ae989a2e42ee09d7ad60bd3445742b7
Ashutosh0731 <tripathiashutosh480@gmail.com>
2026-07-07 03:25:12 +0530

    Time: 1 ms (97.86%) | Memory: 42.6 MB (62.82%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,27 +1,6 @@
 class Solution {
     public int addDigits(int num) {
-        int temp = 0, sum = 0;
-        int i = 0;
-
-        while (num != 0) {
-            temp = num % 10; // 8,3
-            num = num / 10; // 3,0
-            sum += temp; // 8+3 = 11
-            i++; // 2
-        }
-
-
-        while (sum >= 10) {
-            num = sum;
-            sum = 0;
-
-            while (num != 0) {
-                temp = num % 10; // 1,1
-                num = num / 10; // 1,0
-                sum += temp; // 1+1 = 2
-
-            }
-        }
-        return sum;
+        if (num == 0) return 0;
+        return 1 + (num - 1) % 9;
     }
 }

65688f85118f310d9253cd2484dc42dbb4195cd1
Ashutosh0731 <tripathiashutosh480@gmail.com>
2026-08-28 00:31:43 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (15.97%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,20 +1,27 @@
 class Solution {
+
     public void nextPermutation(int[] nums) {
         int n = nums.length;
+
         for (int i = n - 2; i >= 0; i--) {
+
             if (nums[i + 1] > nums[i]) {
 
                 int j = n - 1;
 
                 while (nums[j] <= nums[i]) {
-                    j--; // bas j ko move karo
+                    j--;
                 }
 
-                swap(nums, i, j); // while KHATAM hone ke baad swap
+                swap(nums, i, j);
+
                 reverse(nums, i + 1, n - 1);
+
                 return;
             }
         }
+
+        // agar pivot nahi mila, matlab array descending order me hai
         reverse(nums, 0, n - 1);
     }
 
@@ -23,11 +30,12 @@ class Solution {
         nums[i] = nums[j];
         nums[j] = temp;
     }
+
     public void reverse(int[] nums, int i, int j) {
-    while (i < j) {
-        swap(nums, i, j);
-        i++;
-        j--;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
     }
-}
 }

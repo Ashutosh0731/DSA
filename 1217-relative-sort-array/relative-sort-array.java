ff802295b033bb5881a5554fa1538df6682fe53a
Ashutosh0731 <tripathiashutosh480@gmail.com>
2026-08-29 01:37:40 +0530

    Time: 5 ms (28.01%) | Memory: 43.4 MB (91.20%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        
+    int n = arr1.length;
+    int m = arr2.length;
+    Arrays.sort(arr1);
+    int k = 0;
+    
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            if(arr2[i] == arr1[j]){
+                int temp = arr1[k];
+                arr1[k] = arr1[j];
+                arr1[j] = temp;
+                k++;
+            }
+            
+        }
+    }
+    Arrays.sort(arr1, k, n);
+    return arr1;
+    }
+        
+}

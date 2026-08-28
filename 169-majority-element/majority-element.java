04fc97926b9a507243cd66073e02323a0812c1b7
Ashutosh0731 <tripathiashutosh480@gmail.com>
2026-08-28 17:50:22 +0530

    Time: 1 ms (99.89%) | Memory: 55.7 MB (31.89%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,17 +1,31 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        // Moore's Voting Alogrithm
         int n = nums.length;
-        // int max = 0;
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                if (nums[i] == nums[j])
-                    count++;
-
-                }
-                if (count > n / 2) {
-                    return nums[i];
+        int freq = 0;
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            if(freq == 0){
+                ans = nums[i];
             }
+            if(ans == nums[i]) freq++;
+            else freq--;
+        }
+        return ans;
+
+
+        // Second Apporoch
+        // int max = 0;
+        // for (int i = 0; i < n; i++) {
+        //     int count = 0;
+        //     for (int j = 0; j < n; j++) { // [2,2,1,1,1,2,2]
+        //         if (nums[i] == nums[j])
+        //             count++;
+
+        //         }
+        //         if (count > n / 2) {
+        //             return nums[i];
+        //         }
 
             // My One approach
 
@@ -31,8 +45,8 @@ class Solution {
             //     }
             // }
             // System.out.println(count);
-        }
+        // }
         // System.out.println(max);
-        return -1;
+        // return -1;
     }
 }

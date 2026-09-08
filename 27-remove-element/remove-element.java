1797aeb293abc6dff52cf656931365dae16ba25f
Ashutosh0731 <tripathiashutosh480@gmail.com>
2026-09-09 00:30:32 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (42.84%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,16 +1,32 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
 
+        int n = nums.length;
         int i = 0;
+        int count = 0;
 
-        for (int j = 0; j < nums.length; j++) {
+        for (int j = 0; j < n; j++) { // fix 1: j=0 se start
             if (nums[j] != val) {
+                int temp = nums[i];
                 nums[i] = nums[j];
+                nums[j] = temp;
                 i++;
+                count = i; // ab har valid element pe update hota hai
             }
         }
 
-        return i;
+        return count;
+
+        // int i = 0;
+
+        // for (int j = 0; j < nums.length; j++) {
+        //     if (nums[j] != val) {
+        //         nums[i] = nums[j];
+        //         i++;
+        //     }
+        // }
+
+        // return i;
 
         // int n = nums.length;
 

5313ebb9ec4761a3db17a3391fa4a88dff954f49
Ashutosh0731 <tripathiashutosh480@gmail.com>
2026-09-10 19:23:24 +0530

    Time: 3 ms (0.80%) | Memory: 43.5 MB (58.31%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -1,40 +1,62 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        // More Clean Code
         if (head == null)
             return head;
 
-        ListNode temp = head;
-        if (n == 1) {
-            if (temp.next == null) {
-                head = null;
-                return head;
-            }
-        }
+        // 1. Length find kara
         ListNode tail = head;
         int length = 0;
+
         while (tail != null) {
             tail = tail.next;
             length++;
         }
-        if(n == length){
+
+        // 2. Agar first node hi delete karna hai toh yeah use use me ayega
+        if (n == length) {
             head = head.next;
             return head;
         }
+
+        // 3. Delete hone wale node ke previous node tak jao
+        ListNode temp = head;
+
         for (int i = 0; i < length - n - 1; i++) {
             temp = temp.next;
         }
-        temp.next = temp.next.next;
 
+        // 4. Node delete
+        temp.next = temp.next.next;
+        System.gc();
         return head;
+
+        // if (head == null)
+        //     return head;
+
+        // ListNode temp = head;
+        // if (n == 1) {
+        //     if (temp.next == null) {
+        //         head = null;
+        //         return head;
+        //     }
+        // }
+        // ListNode tail = head;
+        // int length = 0;
+        // while (tail != null) {
+        //     tail = tail.next;
+        //     length++;
+        // }
+        // if(n == length){
+        //     head = head.next;
+        //     return head;
+        // }
+        // for (int i = 0; i < length - n - 1; i++) {
+        //     temp = temp.next;
+        // }
+        // temp.next = temp.next.next;
+
+        // return head;
     }
 }

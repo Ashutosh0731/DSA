a5b0496bf274aff5648267eec9444bba912a96b6
Ashutosh0731 <tripathiashutosh480@gmail.com>
2026-09-10 23:29:25 +0530

    Time: 4 ms (0.89%) | Memory: 43.9 MB (99.96%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.java
+++ 2048-build-array-from-permutation/build-array-from-permutation.java
@@ -10,7 +10,7 @@ class Solution {
         for(int i = 0; i < n; i++) {
             nums[i] = nums[i] / n;
         }
-
+        System.gc();
         return nums;
         
         // int[] ans = nums.clone();

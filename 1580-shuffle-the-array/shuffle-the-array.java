b190b6544f287ac85c4eada61f3e2b61f2c91460
Ashutosh0731 <tripathiashutosh480@gmail.com>
2026-09-10 23:08:41 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (41.13%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,14 +1,17 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int len = nums.length;
-        int[] ans = new int[len];
-        for (int i = 0; i < len; i++) {
-            if (i % 2 == 0) {
-                // Even positions get elements from the first half
-                ans[i] = nums[i / 2];
-            } else {
-                // Odd positions get elements from the second half (starting at index n)
-                ans[i] = nums[n + (i / 2)];
+
+        int[] ans = new int[nums.length];
+        int j = 0;
+
+        for(int i = 0; i < ans.length; i++){
+            if(i % 2 == 0){
+                ans[i] = nums[j];
+                j++;
+            }
+            else{
+                ans[i] = nums[n];
+                n++;
             }
         }
         return ans;

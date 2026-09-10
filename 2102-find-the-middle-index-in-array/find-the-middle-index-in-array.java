3504ef32dbc32a5d2db9c6188a2bd30fdd722787
Ashutosh0731 <tripathiashutosh480@gmail.com>
2026-09-11 02:06:34 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (58.79%) - LeetSync

--- 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.java
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.java
@@ -1,24 +1,48 @@
 class Solution {
     public int findMiddleIndex(int[] nums) {
-                 int n = nums.length;
-        for(int i = 0; i < n; i++){
-            int left = 0;
-            int right = i+1;
-            int rightSum = 0;
-            int leftSum = 0;
-            while(left < right && left != i){
-                leftSum += nums[left];
-                left++;
-            }
-            while(right < n && right != i){
-                rightSum += nums[right];
-                right++;
-            }
-            if(rightSum == leftSum){
+
+        int totalSum = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            totalSum += nums[i];
+        }
+
+        int leftSum = 0;
+
+        for(int i = 0; i < nums.length; i++){
+
+            int rightSum = totalSum - leftSum - nums[i];
+
+            if(leftSum == rightSum){
                 return i;
             }
 
+            leftSum += nums[i];
         }
+
         return -1;
+
+
+
+        // int n = nums.length;
+        // for(int i = 0; i < n; i++){
+        //     int left = 0;
+        //     int right = i+1;
+        //     int rightSum = 0;
+        //     int leftSum = 0;
+        //     while(left < right && left != i){
+        //         leftSum += nums[left];
+        //         left++;
+        //     }
+        //     while(right < n && right != i){
+        //         rightSum += nums[right];
+        //         right++;
+        //     }
+        //     if(rightSum == leftSum){
+        //         return i;
+        //     }
+
+        // }
+        // return -1;
     }
 }

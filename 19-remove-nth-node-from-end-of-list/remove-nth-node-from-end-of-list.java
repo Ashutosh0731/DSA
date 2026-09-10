ed0593947fc9809ae13fe75c843402c86c9cedd6
Ashutosh0731 <tripathiashutosh480@gmail.com>
2026-09-10 19:30:17 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (58.31%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -1,36 +1,61 @@
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
 
+        // One Pass Solution
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        // fast ko n+1 steps aage le jayenge
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        // dono ko saath move karenge
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        // slow ke next node ko delete karenge
+        slow.next = slow.next.next;
+
+        return dummy.next;
+
+
         // More Clean Code
-        if (head == null)
-            return head;
+        // if (head == null)
+        //     return head;
 
-        // 1. Length find kara
-        ListNode tail = head;
-        int length = 0;
+        // // 1. Length find kara
+        // ListNode tail = head;
+        // int length = 0;
 
-        while (tail != null) {
-            tail = tail.next;
-            length++;
-        }
+        // while (tail != null) {
+        //     tail = tail.next;
+        //     length++;
+        // }
 
-        // 2. Agar first node hi delete karna hai toh yeah use use me ayega
-        if (n == length) {
-            head = head.next;
-            return head;
-        }
+        // // 2. Agar first node hi delete karna hai toh yeah use use me ayega
+        // if (n == length) {
+        //     head = head.next;
+        //     return head;
+        // }
 
-        // 3. Delete hone wale node ke previous node tak jao
-        ListNode temp = head;
+        // // 3. Delete hone wale node ke previous node tak jao
+        // ListNode temp = head;
 
-        for (int i = 0; i < length - n - 1; i++) {
-            temp = temp.next;
-        }
+        // for (int i = 0; i < length - n - 1; i++) {
+        //     temp = temp.next;
+        // }
 
-        // 4. Node delete
-        temp.next = temp.next.next;
-        return head;
+        // // 4. Node delete
+        // temp.next = temp.next.next;
+        // return head;
 
+        // Correct Code But Not Clean
         // if (head == null)
         //     return head;
 

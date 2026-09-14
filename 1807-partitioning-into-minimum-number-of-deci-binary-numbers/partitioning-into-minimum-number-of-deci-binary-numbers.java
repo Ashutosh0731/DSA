17cde92743831e288f5cac66955fab166ca13823
Ashutosh0731 <tripathiashutosh480@gmail.com>
2026-09-15 03:49:20 +0530

    Time: 6 ms (65.42%) | Memory: 47.8 MB (15.72%) - LeetSync

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.java
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -1,18 +1,23 @@
 class Solution {
     public int minPartitions(String n) {
+        int maxi = 0;
+        for(int i = 0 ; i < n.length() ; i++){
+            maxi = Math.max(maxi , n.charAt(i) - '0');
+        }
+        return maxi;
 
-    if(n.contains("9")) return 9;
-    else if(n.contains("8")) return 8;
-    else if(n.contains("7")) return 7;
-    else if(n.contains("6")) return 6;
-    else if(n.contains("5")) return 5;
-    else if(n.contains("4")) return 4;
-    else if(n.contains("3")) return 3;
-    else if(n.contains("2")) return 2;
-    else if(n.contains("1")) return 1;
-    else return 0;
+    // if(n.contains("9")) return 9;
+    // else if(n.contains("8")) return 8;
+    // else if(n.contains("7")) return 7;
+    // else if(n.contains("6")) return 6;
+    // else if(n.contains("5")) return 5;
+    // else if(n.contains("4")) return 4;
+    // else if(n.contains("3")) return 3;
+    // else if(n.contains("2")) return 2;
+    // else if(n.contains("1")) return 1;
+    // else return 0;
 
-    
+
         // int max = -1;
         // for(int i = 0; i < n.length(); i++){
         //     int temp = n.charAt(i) - 48;

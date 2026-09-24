2501f02b7cc55c4168de49847e9e9bcc8a1e718f
Ashutosh0731 <tripathiashutosh480@gmail.com>
2026-09-24 23:01:31 +0530

    Time: 1 ms (99.69%) | Memory: 45.4 MB (77.88%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
@@ -1,20 +1,40 @@
 class Solution {
     public int smallestIndex(int[] nums) {
-        int n = nums.length;
-        int min = -1;
-        for(int i = 0; i < n; i++){
+
+        for (int i = 0; i < nums.length; i++) {
+
+        int n = nums[i];
             int sum = 0;
-            int a = nums[i];
-            while(a != 0){
-                int lastDig = a % 10;
-                sum += lastDig;
-                a = a/10;
+
+            while (n > 0) {
+                int digit = n % 10;
+                sum = sum + digit;
+                n = n / 10;
             }
-            if(sum == i){
+
+            if (sum == i) {
                 return i;
-            } 
-            System.out.println("sum -> "+sum+" i->"+i);
+            }
+        
         }
-        return min;
+        return -1;
+
+
+        // int n = nums.length;
+        // int min = -1;
+        // for(int i = 0; i < n; i++){
+        //     int sum = 0;
+        //     int a = nums[i];
+        //     while(a != 0){
+        //         int lastDig = a % 10;
+        //         sum += lastDig;
+        //         a = a/10;
+        //     }
+        //     if(sum == i){
+        //         return i;
+        //     } 
+        //     System.out.println("sum -> "+sum+" i->"+i);
+        // }
+        // return min;
     }
 }
